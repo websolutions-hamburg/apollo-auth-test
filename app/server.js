794c0edaf1ee5df9abea75f5ddfa7e94449355fe
Robert Hädrich <mail@websolutions.hamburg>
2019-07-10 22:28:00 +0200

    init

---
+++ app/server.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+    Query: {
+        hello: () => 'world',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: {
+        config: {
+            projects: {
+                Middleware: {
+                    extensions: {
+                        endpoints: {
+                            'node-local': {
+                                url: 'http://node.local'
+                            },
+                            'node2-local': {
+                                url: 'http://node2.local',
+                                headers: {
+                                    Authorization: 'Basic dGVzdDp0ZXN0'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
